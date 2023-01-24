1467fbde0945fc0c4e71335840d1591fa7cec645
ryoum-0930 <dara.ryouma@gmail.com>
2023-01-24 14:08:30 +0900

    try test code

--- srial_recv.py
+++ srial_recv.py
@@ -6,8 +6,13 @@ if __name__=='__main__':
     PORTNAME='COM5'
     sc.open(PORTNAME)
     while(1):
-        res,data = sc.recv(5)
-        # 末尾が:endかどうか
-        if data[-4:]==":end":
-            print(res,data)
+        # res,data = sc.recv(5)
+        data = "date;type;level;area;detail;end".encode()
+        # 末尾が:endかどうか バイナリで判別する必要があるか検証
+        if data[-4:]==";end".encode():
+        # if data.hex()[-8:]=='3b656e64':
+            # print(res,data)
+            data = data.decode().split(';')
+            print(data)
+            break
         time.sleep(0.1)
